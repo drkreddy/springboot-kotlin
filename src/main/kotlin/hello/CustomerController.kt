2cf3e078a1d71832e377d2bd8e8026c66c66b985
Donapati Ravindra Kumar Reddy <util4ravi@gmail.com>
2018-06-06 07:46:55 +0530

    changed

--- src/main/kotlin/hello/CustomerController.kt
+++ src/main/kotlin/hello/CustomerController.kt
@@ -2,15 +2,18 @@ package hello
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
 @RestController
+@RequestMapping("/customers")
 class CustomerController(private val repository: CustomerRepository) {
 
-	@GetMapping("/customers")
+	@GetMapping()
 	fun findAll() = repository.findAll()
 
-	@GetMapping("/customers/{lastName}")
-	fun findByLastName(@PathVariable lastName:String)
-			= repository.findByLastName(lastName)
+	@PostMapping()
+	fun post(@Valid cust: CustomerRequest) = "done"
 }
---
+++ src/main/kotlin/hello/CustomerRequest.kt
@@ -0,0 +1,12 @@
+package hello
+
+class CustomerRequest(
+
+        val firstName: String,
+
+        val lastName: String,
+
+        val income: Int,
+
+        val hasLoan: Boolean
+)
